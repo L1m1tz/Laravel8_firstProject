140d4e218006d19c82a3bb5282113e87f8f2e449
L1m1tz <andreew.mk@gmail.com>
2021-09-20 13:45:19 +0200

    Learning Routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,28 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return env('DB_DATABASE');
+    return view('welcome');
+});
+
+//Route to users
+Route::get('/users', function () {
+    return 'Welcome to the users page';
+});
+
+//Route users - Array(JSON)
+Route::get('/users', function () {
+    return ['PHP', 'HTML', 'Laravel'];
+});
+
+//Route to users - JSON object
+Route::get('/users', function () {
+    return response()->json([
+        'name' => 'Andrew',
+        'course' => 'Laravel Beginners To Advanced'
+    ]);
+});
+
+//Route to users - function
+Route::get('/users ', function () {
+    return redirect('/');
 });
